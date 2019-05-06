3ea8b74fefb909dc222c8dca8003c3c96973efda
davidmfritz <david.m.fritz@gmail.com>
2019-05-06 22:34:13 +0200

    Added Link at removing an Entry if Forward fails

--- html/gb/rmentry.php
+++ html/gb/rmentry.php
@@ -6,11 +6,15 @@ if (isset($id)) {
     $sql2 = "DELETE from posts WHERE PostID = " . $id;
     if (@mysql_query($sql2)) {
         if (mysql_affected_rows() == 1) {
-            echo ("<p>Eintrag " . $id . " gel�scht</p>");
-            echo "<meta http-equiv='refresh' content='2; URL=../?p=gb_acp'>";
+            $fw_delay = 2;
+            echo "<p>Eintrag " . $id . " gelöscht</p>";
+            echo "Sie werden in $fw_delay Sekunden automatisch weitergeleitet. <a href='../?p=gb_acp'>Zurück zum ACP</a>";
+            echo "<meta http-equiv='refresh' content='$fw_delay; URL=../?p=gb_acp'>";
         } else {
-            echo ("<p><b>FEHLER beim l�schen, dieser Datensatz existiert <b>nicht: <u>" . $id . "</u></b></p>");
-            echo "<meta http-equiv='refresh' content='5; URL=../?p=gb_acp'>";
+            $fw_delay = 5;
+            echo ("<p><b>FEHLER beim löschen, dieser Datensatz existiert <b>nicht: <u>" . $id . "</u></b></p>");
+            echo "Sie werden in $fw_delay Sekunden automatisch weitergeleitet. <a href='../?p=gb_acp'>Zurück zum ACP</a>";
+            echo "<meta http-equiv='refresh' content='$fw_delay; URL=../?p=gb_acp'>";
         }
     } else {
         echo "Kein passender Datensatz gefunden.";
